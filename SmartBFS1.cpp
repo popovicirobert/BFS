0650ea5038f231b8b6d0d1908adeec51e527e704
Student USO VM User <student@stud.acs.upb.ro>
2020-10-23 17:33:59 +0300

    fixed SmartBFS1

--- SmartBFS1.cpp
+++ SmartBFS1.cpp
@@ -55,7 +55,7 @@ static inline const int GetNr() {
 constexpr int MAXN = (1 << 16);
 constexpr int MAXM = (1 << 19);
 
-static atomic<int> degree[MAXN];
+static int degree[MAXN];
 static unsigned short* graph[MAXN];
 static pair<unsigned short, unsigned short> edge[MAXM];
 
@@ -74,9 +74,12 @@ static inline void ReadInput(int& n, int& m) {
 				assert(b > -1);	
 			#endif
 
-			degree[a]++;
-			degree[b]++;
-			edge[m++] = {a, b};
+			++degree[a];
+			++degree[b];
+
+			edge[m] = {a, b};
+			++m;
+
 			n = max(n, max(a, b));
 		}
 	}	
@@ -88,67 +91,53 @@ static inline void ReadInput(int& n, int& m) {
 			degree[i] = 0;
 		}
 	}
-	
-	#pragma omp parallel for
+
 	for(int i = 0; i < m; ++i) {
-		graph[edge[i].first][degree[edge[i].first]++] = edge[i].second;
-		graph[edge[i].second][degree[edge[i].second]++] = edge[i].first;
+		graph[edge[i].first][degree[edge[i].first]] = edge[i].second;
+		++degree[edge[i].first];
+		graph[edge[i].second][degree[edge[i].second]] = edge[i].first;
+		++degree[edge[i].second];
 	}
 }
 
 
-constexpr int BUCKET_SIZE = 6; // 1 << 6 = 64
-
-static atomic<unsigned long long> visited[MAXN / (1 << BUCKET_SIZE)];
-static unsigned short nodes[2][MAXN];
-
-static inline const bool isVisited(const unsigned short nod) {
-	const unsigned short id = (nod >> BUCKET_SIZE);
-	const char rem = (nod & ((1 << BUCKET_SIZE) - 1));
-	return (visited[id] & (1llu << rem)) > 0;
-}
-
-static inline void VisitNode(const unsigned short nod) {
-	const unsigned short id = (nod >> BUCKET_SIZE);
-	const char rem = (nod & ((1 << BUCKET_SIZE) - 1));
-	visited[id] |= (1llu << rem);
-}
+static unsigned short visited[2][MAXN];
 
 
 static inline void Solve(int n, int m) {
-	static atomic<int> sz[2];
-	sz[1] = 0;
-		
-	bool t = 0;
 
-	visited[0] = 1;
-	sz[0] = 1;
+	unsigned short dist = 1;
+	visited[0][0] = 1;
 
-	while(sz[t]) {
-		sz[t ^ 1] = 0;
+	bool found = true;
+
+	while(found) {
+		found = false;	
 
 		#pragma omp parallel for
-		for(int i = 0; i < sz[t]; ++i) {
-			#pragma omp parallel for
-			for(int j = 0; j < degree[nodes[t][i]]; ++j) {
-				unsigned short new_nod = graph[nodes[t][i]][j];
-
-				#ifdef HOME
-					assert(new_nod <= n);
-				#endif
-
-				if(!isVisited(new_nod)) {
-					VisitNode(new_nod);
-					nodes[t ^ 1][sz[t ^ 1]] = new_nod;
-					sz[t ^ 1]++;
+		for(int nod = 0; nod <= n; nod++) {
+			if(visited[0][nod] == dist) {
+				//#pragma omp parallel for
+				for(int i = 0; i < degree[nod]; i++) {
+					if(visited[0][graph[nod][i]] == 0) {
+						visited[1][graph[nod][i]] = dist + 1;
+						found = true;
+					}
 				}
 			}
 		}
 
-		sz[t] = 0;
-		t ^= 1;
+		#pragma omp parallel for
+		for(int nod = 0; nod <= n; nod++) {
+			if(visited[1][nod]) {
+				visited[0][nod] = visited[1][nod];
+			}
+		}
+
+		dist++;
 	}
 }
+
 
 
 static inline void PrintSol(int n, int m) {
@@ -156,21 +145,14 @@ static inline void PrintSol(int n, int m) {
 
 	printf("[");
 	
-	const int lim = (n >> BUCKET_SIZE) + 1;
-	int bit;
-	
-	for(int i = 0; i < lim; ++i) {
+	for(int i = 0; i <= n; i++) {
 		if(visited[i]) {
-			for(bit = 0; bit < (1 << BUCKET_SIZE); ++bit) {
-				if(visited[i] & (1llu << bit)) {
-					if(first) {
-						printf("%hd", (i << BUCKET_SIZE) + bit);
-						first = false;
-					}
-					else {
-						printf(",%hd", (i << BUCKET_SIZE) + bit);
-					}
-				}
+			if(first) {
+				first = false;
+				printf("%hd", i);
+			}
+			else {
+				printf(",%hd", i);
 			}
 		}
 	}
