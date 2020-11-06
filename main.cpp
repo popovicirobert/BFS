87473422ef90911783bfe16586650a2c829f4d20
Student USO VM User <student@stud.acs.upb.ro>
2020-11-06 19:39:55 +0200

    bitset

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <future>
-#include <chrono>
-#include <random>
+#include <bitset>
 
 #define MAXBUF (1 << 17)
-#define SIGMA 256
 
 static char buf[MAXBUF];
 int pbuf;
@@ -44,149 +41,105 @@ static inline const short GetNr() {
 }
 
 
-#define MAXN (1 << 15) // consideram ca numerele sunt pe short
-#define MAXM (int)(5e6 + 5)
+#define MAXN (1 << 14) // consideram ca numerele sunt pe short
+
+static std::bitset<MAXN> edge[MAXN];
+
+const int MAXM = (int) 5e5 + 5;
 
 static short x[MAXM], y[MAXM];
-static int nxt[MAXM], last[MAXN];
-
-static inline int max(const int a, const int b) {
-	if(a > b) return a;
-	return b;
-}
-
-static inline int min(const int a, const int b) {
-	if(a < b) return a;
-	return b;
-}
-
-short n;
 int m;
 
-short func(int l, int r) {
-	int ans = 0;
-	for(int i = l; i <= r; ++i) {
-		ans = max(ans, max(x[i], y[i]));
-	}
-	return ans;
-}
-
 static inline void ReadInput() {
-
 	while(1) {
-		++m;
-		x[m] = GetNr();
+		x[++m] = GetNr();
 		if(x[m] > -1) {
 			y[m] = GetNr();
+			edge[x[m]][y[m]] = 1;
 		}
 		else {
 			break;
 		}
-	}	
-	m--;
-
-	int len = m / 90 + 1;
-	for(int i = 0; i * len < m; i++) {
-		int l = i * len + 1;
-		int r = min((i + 1) * len, m);
-		auto cur = std::async(std::launch::async, func, l, r);
-		n = max(n, cur.get());
-	}
-	
-	if(m <= 2 * MAXN) {
-		for(int i = m; i >= 1; --i) {
-			nxt[i] = last[x[i]];
-			last[x[i]] = i;
-		}
-	}
-	else {
-		std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-		int arr[128];
-		for(int i = 0; i < 128; i++) {
-			arr[i] = rng() % 97;
-		}
-		for(int i = m; i > m - 128; i--) {
-			nxt[i] = last[x[i]];
-			last[x[i]] = i;
-		}	
-		for(int i = m - 128; i >= 129; i--) {
-			if(arr[(i + (i >> 7)) & 127]) {
-				nxt[i] = last[x[i]];
-				last[x[i]] = i;
-			}
-		}
-		for(int i = 128; i >= 1; i--) {
-			nxt[i] = last[x[i]];
-			last[x[i]] = i;
-		}
-	}
+	}		
 }
 
 
-static char visited[MAXN];
+static std::bitset<MAXN> notVisited, temp;
 static short Q[MAXN];
 
-int sz = 0;
+static short smallX[MAXN], smallY[MAXN];
+static short last[MAXN], nxt[MAXN];
+short n = 0;
 
-static inline void AddChar(const char ch) {
-	buf[sz++] = ch;
-	if(sz == MAXBUF) {
-		fwrite(buf, 1, MAXBUF, stdout);
-		sz = 0;
+
+static inline void GetEdges() {
+	notVisited.set();
+
+	short l = 0, r = 1;
+	Q[0] = 0;
+	notVisited[0] = 0;
+
+	n = 0;
+
+	while(l < r) {
+		short nod = Q[l];
+		l++;
+
+		temp = (edge[nod] & notVisited);
+		short pos = temp._Find_first();
+
+		while(pos != MAXN) {
+			notVisited[pos] = 0;
+			Q[r++] = pos;
+			
+			smallX[n] = nod;
+			smallY[n] = pos;
+			n++;
+
+			pos = temp._Find_next(pos);
+		}
 	}
+	
+	for(short i = 0; i < MAXN; i++) {
+		edge[i].reset();
+	}
+	for(short i = 0; i < n; i++) {
+		edge[smallX[i]][smallY[i]] = 1;
+	}
+	short sz = n;
+	for(int i = m; i >= 1; i--) {
+		if(edge[x[i]][y[i]]) {
+			nxt[sz] = last[x[i]];
+			last[x[i]] = sz;
+
+			smallY[sz] = y[i];
+			sz--;
+		}
+	}
+	n++;
 }
 
 
 static inline void Solve() {
-
 	short l = 0, r = 1;
-	Q[0] = 0;
-	visited[0] = 1;
 
-	AddChar('[');
-	AddChar('0');
-	
-	int pos;
+	Q[0] = 0;
+	printf("[0");
 
 	while(l < r) {
-		pos = last[Q[l]];
-		++l;
-
+		short nod = Q[l];
+		l++;
+	
+		short pos = last[nod];
 		while(pos) {
-			if(visited[y[pos]] == 0) {
-				visited[y[pos]] = 1;
-				Q[r] = y[pos];
-				++r;
-
-				AddChar(',');
-
-				if(y[pos] == 0) {
-					AddChar('0');
-				}
-				else {
-					short cur = y[pos];
-					for(short pw = 10000; pw >= 1; pw /= 10) {
-						if(cur >= pw) {
-							AddChar('0' + (cur / pw) % 10);
-						}
-					}
-				}
-
-				if(r == n + 1) {
-					AddChar(']');
-					if(sz) {
-						fwrite(buf, 1, sz, stdout);
-					}
-					return;
-				}
+			Q[r++] = smallY[pos];
+			printf(",%d", smallY[pos]);
+			if(r == n) {
+				printf("]");
+				return;
 			}
 			pos = nxt[pos];
 		}
-	}
-	AddChar(']');
-
-	if(sz) {
-		fwrite(buf, 1, sz, stdout);
 	}
 }
 
@@ -197,6 +150,8 @@ int main() {
 	Init();
 
 	ReadInput();
+
+	GetEdges();
 
 	Solve();
 
