ef173f5703175846535bf607d5e6ece7fb8d98fc
popovicirobert <popovicirob01@gmail.com>
2020-11-29 16:10:39 +0200

    0.293 s

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
-//#include <omp.h>
+
 #define ull unsigned long long
 
 #define MAXBUF (1 << 17)
@@ -47,7 +47,7 @@ static inline int max(int a, int b) {
 
 
 #define MAXN 13000
-#define MAXM 300000
+#define MAXM 90000
 
 static short x[MAXM], y[MAXM];
 static int degree[MAXN];
@@ -58,9 +58,9 @@ static short goodNodes[MAXN];
 int size;
 
 static char startPoint[MAXN];
+static char endPoint[MAXN];
 
 int n, m;
-//int maxGoodNodes;
 
 static inline void ReadInput() {
 	while(1) {
@@ -68,8 +68,6 @@ static inline void ReadInput() {
 		x[m] = GetNr();
 		if(x[m] > -1) {
 			y[m] = GetNr();
-			degree[x[m]]++;
-			degree_rev[y[m]] = 1;
 		}
 		else {
 			break;
@@ -77,16 +75,22 @@ static inline void ReadInput() {
 	}	
 	m--;
 
-	for(int i = 0; i < MAXN; i++) {
-		if(degree[i] || degree_rev[i]) {
-			n = max(n, i);
-			if(degree[i] && degree_rev[i]) {
-				goodNodes[size++] = i;
-				//maxGoodNodes = max(maxGoodNodes, i);
-			}
+	for(int i = 0; i <= m; ++i) {
+		degree[x[i]]++;
+		degree_rev[y[i]] = 1;
+	}
+
+	n = MAXN - 1;
+	while(degree[n] == 0 && degree_rev[n] == 0) {
+		--n;
+	}
+
+	for(int i = 0; i <= n; ++i) {
+		if(degree[i] && degree_rev[i]) {
+			goodNodes[size++] = i;
 		}
 	}
-	for(int i = 1; i <= n + 1; i++) {
+	for(int i = 1; i <= n + 1; ++i) {
 		degree[i] += degree[i - 1];
 	}
 
@@ -107,26 +111,35 @@ static inline void ReadInput() {
 	}
 }
 
-const int INF = 17;
+const int INF = 17; // merge aparent si cu 10
 
 static float g[MAXN];
 
 #include <string.h>
 
 static inline void Solve() {
-	#pragma omp parallel for schedule(dynamic) num_threads(42)
+
+	#pragma omp parallel for schedule(dynamic) num_threads(44)
 	for(int s = 0; s <= n; s++) {
 
 		if(!startPoint[s]) continue;
+
+		const float inv[32] = {0, 1, 0.5, 0.333333,
+							   0.25, 0.2, 0.166667,
+							   0.142857, 0.125, 0.111111,
+							   0.1, 0.0909091, 0.0833333, 0.0769231,
+							   0.0714286, 0.0666667, 0.0625, 0.0588235,
+							   0.0555556, 0.0526316, 0.05, 0.047619,
+							   0.0454545, 0.0434783, 0.0416667, 0.04,
+							   0.0384615, 0.037037, 0.0357143, 0.0344828,
+							   0.0333333, 0.0322581};
 
 		unsigned char dist[MAXN];
 		short ways0[MAXN];
 		float ways1[MAXN];
 		short Q[MAXN];
-		float G[MAXN];
 
 		memset(dist, 0, sizeof(unsigned char) * (n + 1));
-		memset(G, 0, sizeof(float) * (n + 1));
 
 		int l = 0, r = 1;
 
@@ -168,6 +181,10 @@ static inline void Solve() {
 			}
 		}
 
+		if(size < r) {
+			memset(ways1, 0, sizeof(float) * (n + 1));
+		}
+
 		for(int i = r - 1; i >= 1; --i) {
 			const int v = Q[i];
 			float sum = 0;
@@ -178,19 +195,50 @@ static inline void Solve() {
 				pos = nxt[pos];
 			}
 
-			ways1[v] = sum;
-
-			G[v] = ways0[v] * ways1[v];
-
-			ways1[v] += 1.0 / ways0[v];
+			ways1[v] = sum + inv[ways0[v]];
 		}
 
-		#pragma omp critical
-		{
+		int pos = 0;
+		for(int i = 1; i < r; ++i) {
+			if(!endPoint[Q[i]]) {
+				Q[pos++] = Q[i];
+			}
+		}
+		r = pos;
+
+		if(size < r) {
 			for(int i = 0; i < size; ++i) {
 				const int nod = goodNodes[i];
-				g[nod] += G[nod];
+				
+				if(ways1[nod]) {
+					ways1[nod] *= ways0[nod];
+					ways1[nod]--;
+				}
 			}
+
+			#pragma omp critical
+			{
+				for(int i = 0; i < size; ++i) {
+					const int nod = goodNodes[i];
+					g[nod] += ways1[nod];
+				}
+			}
+		}
+		else {
+			for(int i = 1; i < r; ++i) {
+				const int nod = Q[i];
+				
+				ways1[nod] *= ways0[nod];
+				ways1[nod]--;
+			}
+
+			#pragma omp critical
+			{
+				for(int i = 1; i < r; ++i) {
+					const int nod = Q[i];
+					g[nod] += ways1[nod];
+				}
+			}	
 		}
 	}
 
---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int arr[10];
+	memset(arr, 1, sizeof(arr));
+
+	for(int i = 0; i < 10; i++) {
+		cerr << arr[i] << " ";
+	}
+	
+	return 0;
+}
